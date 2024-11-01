2871ef16764ce8f6614666a1b0cfc2f22c7a1e3c
masonFei <994085848@qq.com>
2024-11-01 22:53:58 +0800

    让方法参数具备行为能力

--- src/main/java/org/example/java8/FilterApple.java
+++ src/main/java/org/example/java8/FilterApple.java
@@ -21,12 +21,69 @@ public class FilterApple {
         return list;
     }
 
-    public static void main(String[] args) {
-        List<Apple> list = Arrays.asList(new Apple("green", 150), new Apple("red", 200), new Apple("green", 100));
-        List<Apple> greenApples = findGreenApple(list);
-        assert greenApples.size() == 2;
+    public static List<Apple> findApples(List<Apple> apples, String color) {
+        List<Apple> list = new ArrayList<>();
+        for (Apple apple : apples) {
+            if (color.equals(apple.getColor())) {
+                list.add(apple);
+            }
+        }
 
-        System.out.println(greenApples);
+        return list;
+    }
+
+
+    public interface AppleFilter {
+        boolean filter(Apple apple);
+    }
+
+    public static List<Apple> findApples(List<Apple> apples, AppleFilter appleFilter) {
+        List<Apple> list = new ArrayList<>();
+        for (Apple apple : apples) {
+            if (appleFilter.filter(apple)) {
+                list.add(apple);
+            }
+        }
+
+        return list;
+    }
+
+    public static class GreenAnd150WeightFilter implements AppleFilter {
+        @Override
+        public boolean filter(Apple apple) {
+            return apple.getColor().equals("green") && apple.getWeight() > 150;
+        }
+    }
+
+    public static class YellowLess150WeightFilter implements AppleFilter {
+        @Override
+        public boolean filter(Apple apple) {
+            return apple.getColor().equals("yellow") && apple.getWeight() < 150;
+        }
+    }
+
+    public static void main(String[] args) {
+        List<Apple> list = Arrays.asList(new Apple("green", 160), new Apple("red", 200), new Apple("green", 100), new Apple("yellow", 100));
+//        List<Apple> greenApples = findGreenApple(list);
+//        assert greenApples.size() == 2;
+
+//        List<Apple> greenApples = findApples(list, "green");
+//
+//        System.out.println(greenApples);
+
+        List<Apple> greens = findApples(list, new GreenAnd150WeightFilter());
+        System.out.println(greens);
+        List<Apple> yellows = findApples(list, new YellowLess150WeightFilter());
+        System.out.println(yellows);
+
+        List<Apple> reds = findApples(list, new AppleFilter() {
+            @Override
+            public boolean filter(Apple apple) {
+                return "red".equals(apple.getColor());
+            }
+        });
+        System.out.println(reds);
+
     }
 
 }
