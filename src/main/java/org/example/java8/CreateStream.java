0d7bb37c67615d795234dd68fe4226f0609ad719
masonFei <994085848@qq.com>
2024-11-13 23:34:00 +0800

    如何创建stream上集

---
+++ src/main/java/org/example/java8/CreateStream.java
@@ -0,0 +1,54 @@
+package org.example.java8;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class CreateStream {
+
+
+    public static void main(String[] args) {
+        createStreamFromCollection().forEach(System.out::println);
+        System.out.println("======================================");
+        createStreamFromValues().forEach(System.out::println);
+        System.out.println("======================================");
+        createStreamFromArrays().forEach(System.out::println);
+        System.out.println("======================================");
+        Stream<String> streamFromFile = createStreamFromFile();
+        System.out.println(streamFromFile);
+    }
+
+
+    private static Stream<String> createStreamFromCollection() {
+        List<String> list = Arrays.asList("hello", "alex", "wangwenjun", "world");
+        return list.stream();
+    }
+
+
+    private static Stream<String> createStreamFromValues() {
+        return Stream.of("hello", "alex", "wangwenjun", "world");
+    }
+
+
+    private static Stream<String> createStreamFromArrays() {
+        return Arrays.stream(new String[]{"hello", "alex", "wangwenjun", "world"});
+    }
+
+
+    private static Stream<String> createStreamFromFile() {
+
+        Path path = Paths.get("F:\\code\\java\\java8-sharing\\src\\main\\java\\org\\example\\java8\\CreateStream.java");
+
+        try (Stream<String> lines = Files.lines(path)) {
+            lines.forEach(System.out::println);
+            return lines;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+}
