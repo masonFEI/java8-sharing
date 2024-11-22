f551cdd9888f12daa5cdc8b3d25853b17d79127c
masonFei <994085848@qq.com>
2024-11-22 23:16:03 +0800

    Stream综合练习

---
+++ src/main/java/org/example/java8/Trader.java
@@ -0,0 +1,16 @@
+package org.example.java8;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Trader {
+
+
+    private String name;
+    private String city;
+
+
+}
