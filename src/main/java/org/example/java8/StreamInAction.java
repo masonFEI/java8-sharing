ee6d9bc9a261ac477d88e95562fd677cb796c127
masonFei <994085848@qq.com>
2024-11-23 22:37:58 +0800

    Stream综合练习

---
+++ src/main/java/org/example/java8/StreamInAction.java
@@ -0,0 +1,14 @@
+package org.example.java8;
+
+public class StreamInAction {
+
+
+    public static void main(String[] args) {
+
+
+        Trader mario = new Trader("Mario","Milan");
+        Trader alan = new Trader("Alan","Cambridge");
+        Trader brian = new Trader("Brian","Cambridge");
+    }
+
+}
