505018bda66bcd88854b23ec6157a718430e544e
masonFei <994085848@qq.com>
2024-12-05 21:55:06 +0800

    Optional的介绍以及api的详解

--- src/main/java/org/example/java8/OptionalUsage.java
+++ src/main/java/org/example/java8/OptionalUsage.java
@@ -8,7 +8,21 @@ public class OptionalUsage {
     public static void main(String[] args) {
 
         Optional<Insurance> insuranceOptional = Optional.<Insurance>empty();
-        insuranceOptional.get();
+//        insuranceOptional.get();
+
+        Optional<Insurance> insuranceOptional1 = Optional.of(new Insurance());
+        insuranceOptional1.get();
+
+        Optional<Insurance> objectOptional1 = Optional.ofNullable(null);
+        objectOptional1.orElseGet(Insurance::new);
+
+        objectOptional1.orElse(new Insurance());
+
+        objectOptional1.orElseThrow(RuntimeException::new);
+
+        objectOptional1.orElseThrow(()->new RuntimeException("not have reference"));
+
+
     }
 
 }
