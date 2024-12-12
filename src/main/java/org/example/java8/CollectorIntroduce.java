1cc5fb6fded5dbcd6286d1c6442b28c0b1f5d865
feixuanyu <994085848@qq.com>
2024-12-12 22:38:23 +0800

    初识collector

--- src/main/java/org/example/java8/CollectorIntroduce.java
+++ src/main/java/org/example/java8/CollectorIntroduce.java
@@ -25,14 +25,17 @@ public class CollectorIntroduce {
     public static void main(String[] args) {
         List<Apple> list = Arrays.asList(new Apple("green", 160), new Apple("red", 200), new Apple("green", 100), new Apple("yellow", 100));
         List<Apple> greenList = list.stream().filter(a -> a.getColor().equals("green")).collect(Collectors.toList());
-        Optional.ofNullable(greenList).ifPresent(System.out::println);
+//        Optional.ofNullable(greenList).ifPresent(System.out::println);
 
+//        Optional.ofNullable(groupByNormal(greenList)).ifPresent(System.out::println);
+
+        Optional.ofNullable(groupByFunction(list)).ifPresent(System.out::println);
 
     }
 
-    private static Map<String,List<Apple>> groupByNormal(List<Apple> apples){
+    private static Map<String, List<Apple>> groupByNormal(List<Apple> apples) {
         Map<String, List<Apple>> map = new HashMap<>();
-        for(Apple apple : apples){
+        for (Apple apple : apples) {
             List<Apple> list = map.computeIfAbsent(apple.getColor(), k -> new ArrayList<>());
 
             list.add(apple);
@@ -40,5 +43,22 @@ public class CollectorIntroduce {
 
         return map;
     }
+
+    private static Map<String, List<Apple>> groupByFunction(List<Apple> apples) {
+        Map<String, List<Apple>> map = new HashMap<>();
+        apples.stream().forEach(apple ->
+                {
+                    List<Apple> colorList = Optional.ofNullable(map.get(apple.getColor())).orElseGet(() -> {
+                        List<Apple> list = new ArrayList<>();
+                        map.put(apple.getColor(), list);
+                        return list;
+                    });
+                    colorList.add(apple);
+                }
+        );
+
+        return map;
+    }
+
 
 }
