5cadf21e7277135c3aa98a4bb36ed34c638c5358
feixuanyu <994085848@qq.com>
2024-12-28 19:47:34 +0800

    collector使用方法深入详细介绍

---
+++ src/main/java/org/example/java8/CollectorsAction3.java
@@ -0,0 +1,38 @@
+/**
+ * LY.com Inc.
+ * Copyright (c) 2004-2024 All Rights Reserved.
+ */
+package org.example.java8;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import static org.example.java8.CollectorsAction.menu;
+
+/**
+ * CollectorsAction3
+ *
+ * @author johnny.fei
+ * @version 1.0.0
+ * @since 2024-12-28 19:42
+ */
+public class CollectorsAction3 {
+
+
+    public static void main(String[] args) {
+
+        testPartitionByWithPredicate();
+    }
+
+
+    private static void testPartitionByWithPredicate(){
+
+        System.out.println("testPartitionByWithPredicate");
+        Map<Boolean, List<Dish>> collect = menu.stream().collect(Collectors.partitioningBy(Dish::isVegetarian));
+        Optional.of(collect).ifPresent(System.out::println);
+
+    }
+}
