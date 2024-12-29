0367df3b1c4dc69891a5f3eb82fe5a8b9e39ad4d
masonFei <994085848@qq.com>
2024-12-29 10:54:04 +0800

    Collector 使用方法深入详细介绍

--- src/main/java/org/example/java8/CollectorsAction3.java
+++ src/main/java/org/example/java8/CollectorsAction3.java
@@ -7,7 +7,6 @@ package org.example.java8;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import static org.example.java8.CollectorsAction.menu;
@@ -25,14 +24,22 @@ public class CollectorsAction3 {
     public static void main(String[] args) {
 
         testPartitionByWithPredicate();
+        testPartitionByWithPredicateAndCollector();
     }
 
 
-    private static void testPartitionByWithPredicate(){
-
+    private static void testPartitionByWithPredicate() {
         System.out.println("testPartitionByWithPredicate");
         Map<Boolean, List<Dish>> collect = menu.stream().collect(Collectors.partitioningBy(Dish::isVegetarian));
         Optional.of(collect).ifPresent(System.out::println);
 
     }
+
+    private static void testPartitionByWithPredicateAndCollector() {
+        System.out.println("testPartitionByWithPredicateAndCollector");
+        Map<Boolean, Double> collect = menu.stream().collect(Collectors.partitioningBy(Dish::isVegetarian, Collectors.averagingInt(Dish::getCalories)));
+        Optional.of(collect).ifPresent(System.out::println);
+    }
+
+
 }
