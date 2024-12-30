a08dc04d6727c1487da491f1ded3195d5f8c4b8a
masonFei <994085848@qq.com>
2024-12-30 22:13:14 +0800

    Collector 使用方法深入详细介绍

--- src/main/java/org/example/java8/CollectorsAction3.java
+++ src/main/java/org/example/java8/CollectorsAction3.java
@@ -4,9 +4,11 @@
  */
 package org.example.java8;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.function.BinaryOperator;
 import java.util.stream.Collectors;
 
 import static org.example.java8.CollectorsAction.menu;
@@ -23,8 +25,11 @@ public class CollectorsAction3 {
 
     public static void main(String[] args) {
 
-        testPartitionByWithPredicate();
-        testPartitionByWithPredicateAndCollector();
+//        testPartitionByWithPredicate();
+//        testPartitionByWithPredicateAndCollector();
+        testReducingBinaryOperator();
+        testReducingBinaryOperatorAndIdentity();
+        testReducingBinaryOperatorAndIdentityAndFunction();
     }
 
 
@@ -41,5 +46,21 @@ public class CollectorsAction3 {
         Optional.of(collect).ifPresent(System.out::println);
     }
 
+    private static void testReducingBinaryOperator() {
+        System.out.println("testReducingBinaryOperator");
+        menu.stream().collect(Collectors.reducing(BinaryOperator.maxBy(Comparator.comparingInt(Dish::getCalories)))).ifPresent(System.out::println);
+    }
+
+    private static void testReducingBinaryOperatorAndIdentity() {
+        System.out.println("testReducingBinaryOperatorAndIdentity");
+        Integer result = menu.stream().map(Dish::getCalories).collect(Collectors.reducing(0, (d1, d2) -> d1 + d2));
+        System.out.println(result);
+    }
+
+    private static void testReducingBinaryOperatorAndIdentityAndFunction() {
+        System.out.println("testReducingBinaryOperatorAndIdentityAndFunction");
+        Integer result = menu.stream().collect(Collectors.reducing(0, Dish::getCalories, (d1, d2) -> d1 + d2));
+        System.out.println(result);
+    }
 
 }
