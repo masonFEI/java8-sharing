82458e63365f0ebe116387dda7de216d87745393
masonFei <994085848@qq.com>
2025-01-06 22:30:01 +0800

    Collector 使用方法深入详细介绍

--- src/main/java/org/example/java8/CollectorsAction4.java
+++ src/main/java/org/example/java8/CollectorsAction4.java
@@ -10,12 +10,29 @@ public class CollectorsAction4 {
 
     public static void main(String[] args) {
         testSummingDouble();
+        testSummingLong();
+        testSummingInt();
     }
 
 
     private static void testSummingDouble() {
         System.out.println("testSummingDouble");
         Optional.of(menu.stream().collect(Collectors.summingDouble(Dish::getCalories))).ifPresent(System.out::println);
+
+        Optional.of(menu.stream().map(Dish::getCalories).mapToInt(Integer::intValue).sum()).ifPresent(System.out::println);
+
+    }
+
+    private static void testSummingLong() {
+        System.out.println("testSummingLong");
+        Optional.of(menu.stream().collect(Collectors.summingLong(Dish::getCalories))).ifPresent(System.out::println);
+
+    }
+
+    private static void testSummingInt() {
+        System.out.println("testSummingInt");
+        Optional.of(menu.stream().collect(Collectors.summingInt(Dish::getCalories))).ifPresent(System.out::println);
+
     }
 
 
