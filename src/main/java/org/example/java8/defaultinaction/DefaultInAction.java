397713b221c24a470e4f911e92bd27b447e73947
masonFei <994085848@qq.com>
2025-04-05 21:51:01 +0800

    Default方法解决多重继承

--- src/main/java/org/example/java8/defaultinaction/DefaultInAction.java
+++ src/main/java/org/example/java8/defaultinaction/DefaultInAction.java
@@ -4,20 +4,59 @@ public class DefaultInAction {
 
 
     public static void main(String[] args) {
-        A a = () -> 10;
-        System.out.println(a.size());
-        System.out.println(a.isEmpty());
+//        A a = () -> 10;
+//        System.out.println(a.size());
+//        System.out.println(a.isEmpty());
+
+//        DefaultInAction action = new DefaultInAction();
+//        action.confuse(null);
+//
+//        int[] arr = null;
+//        Object o = arr;
+//        action.confuse(o);
+
+        A c = new C();
+        c.hello();
+    }
+
+
+    private void confuse(Object o) {
+        System.out.println("object");
+    }
+
+    private void confuse(int[] i) {
+        System.out.println("int[]");
     }
 
 
     private interface A {
 
-        int size();
+//        int size();
+//
+//        default boolean isEmpty() {
+//            return size() == 0;
+//        }
 
-        default boolean isEmpty() {
-            return size() == 0;
+
+        default void hello() {
+            System.out.println("==A=.hello=");
         }
 
+    }
+
+    private interface B extends A {
+        @Override
+        default void hello() {
+            System.out.println("==B=.hello=");
+        }
+    }
+
+    private static class C implements B, A {
+
+//        @Override
+//        public void hello() {
+//            System.out.println("==C=.hello=");
+//        }
     }
 
 
