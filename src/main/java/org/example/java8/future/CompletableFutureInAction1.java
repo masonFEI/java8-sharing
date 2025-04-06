5462c1fc6ecf574b71b1fbf8fd3f2d3a992a3362
masonFei <994085848@qq.com>
2025-04-06 10:37:54 +0800

    completableFuture用法入门介绍

---
+++ src/main/java/org/example/java8/future/CompletableFutureInAction1.java
@@ -0,0 +1,46 @@
+package org.example.java8.future;
+
+import java.util.Optional;
+import java.util.Random;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+public class CompletableFutureInAction1 {
+
+    private final static Random RANDOM = new Random(System.currentTimeMillis());
+
+
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+
+        CompletableFuture<Double> completableFuture = new CompletableFuture();
+
+        Thread thread = new Thread(() -> {
+            double value = get();
+            completableFuture.complete(value);
+        });
+
+        System.out.println("=======no====block...");
+
+//        Optional.ofNullable(completableFuture.get()).ifPresent(System.out::println);
+
+
+        completableFuture.whenComplete((result, throwable) -> {
+            Optional.ofNullable(result).ifPresent(System.out::println);
+            Optional.ofNullable(throwable).ifPresent(x -> x.printStackTrace());
+        });
+
+
+    }
+
+    private static double get() {
+        try {
+            Thread.sleep(RANDOM.nextInt(10000));
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        return RANDOM.nextDouble();
+    }
+
+
+}
